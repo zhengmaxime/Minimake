4061b7d21b9853a02e4f305675784fc75f59a3d4
Maxime ZHENG <maxime.zheng@epita.fr>
2020-04-23 04:15:58 +0200

    Parser: parse commands

--- src/parser.c
+++ src/parser.c
@@ -6,6 +6,10 @@
 #include <err.h>
 #include "vars_rules.h"
 
+#define UNK 0
+#define VARDEF 1
+#define RULE 2
+
 static inline void skip_comment(char *line)
 {
     char *comment = strchr(line, '#');
@@ -23,16 +27,16 @@ static inline char *skip_spaces(char *line)
     return line;
 }
 
-static char *get_var_value(char *sep)
+static char *get_value(char *line)
 {
-    char *ptr = skip_spaces(sep + 1);
+    char *ptr = skip_spaces(line);
     size_t value_len = strlen(ptr + 1);
     char *value = calloc(1, value_len + 1);
     strncpy(value, ptr, value_len);
     return value;
 }
 
-static inline size_t get_name_len(char *ptr)
+static size_t get_name_len(char *ptr)
 {
     size_t name_len = 0;
     for (; ptr && *ptr != '=' && !isspace(*ptr) && *ptr != ':'; ++ptr)
@@ -60,7 +64,7 @@ static int parse_vardef(struct vars_rules *vr, char *line)
     if (!sep)
         return 0;
 
-    char *value = get_var_value(sep);
+    char *value = get_value(sep + 1);
     char *name = get_name(line);
 
     struct variable *var = variable_init(name, value);
@@ -97,7 +101,33 @@ static int parse_rule(struct vars_rules *vr, char *line)
     struct vec *commands = vec_init(10);
     struct rule *r = rule_init(name, dependencies, commands);
     vr_add_rule(vr, r);
+    return RULE;
+}
+
+static int check_blank_line(char *line)
+{
+    for (; line; ++line)
+    {
+        if (!isspace(*line))
+            return 0;
+    }
     return 1;
+}
+
+static int parse_command(struct vars_rules *vr, char *line)
+{
+    if (check_blank_line(line))
+        return RULE;
+
+    if (line[0] != '\t')
+        return UNK;
+
+    line = skip_spaces(line);
+    char *cmd = get_value(line);
+    struct rule *r = vec_get(vr->rules, vec_size(vr->rules) - 1);
+    vec_add(r->commands, cmd);
+
+    return RULE;
 }
 
 struct vars_rules *parse_file(FILE *f)
@@ -108,14 +138,20 @@ struct vars_rules *parse_file(FILE *f)
     size_t len = 0;
     ssize_t nread;
 
-    int parsing_status;
+    int type = UNK;
 
     while ((nread = getline(&line, &len, f)) != -1)
     {
-        parsing_status = parse_vardef(vr, line);
-        if (parsing_status)
+        if (type == RULE) // EXPECT CMD
+        {
+            type = parse_command(vr, line);
+            if (type == RULE)
+                continue;
+        }
+        type = parse_vardef(vr, line);
+        if (type == VARDEF)
             continue;
-        parsing_status = parse_rule(vr, line);
+        type = parse_rule(vr, line);
     }
     free(line);
 
